221dbc368f7e5d995c5bd594f7d477edabda06cc
Hieu Tran <hieu.trantrung2908@gmail.com>
2023-02-23 20:32:24 +0700

    build NodeJS and MySQL

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,9 +1,32 @@
 const express = require('express')
+var mysql = require('mysql2')
+
 const app = express()
+
 const port = 3000
 
+var con = mysql.createConnection({
+  host: "mysql",
+  port: "3306",
+  user: "root",
+  password: "123456",
+  database: "mydatabase"
+});
+
+con.connect(function(err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+
+  console.log('Connected to the MySQL server.');
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  var sql = "SELECT * FROM users"
+  con.query(sql, function(err, results) {
+    if (err) throw err;
+    res.send(results);
+  });
 })
 
 app.listen(port, () => {
